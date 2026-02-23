f887eda8cb909fd17f766583d2dcb01ef9054375
jamiewilson <jamie@jamiewilson.io>
2026-02-23 17:17:02 -0600

    bugfix (tempates) module resolution ts config
    and remove unnecessary files

--- packages/templates/minimal/env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-	readonly SITE: string
-}
-
-interface ImportMeta {
-	readonly env: ImportMetaEnv
-}
--- packages/templates/minimal/vite.config.ts
+++ packages/templates/minimal/vite.config.ts
@@ -1,7 +1,6 @@
-import { createViteConfig } from '@aero-ssg/config'
-import aeroConfig from './aero.config'
+import { aero } from '@aero-ssg/core/vite'
+import { defineConfig } from 'vite'
 
-export default createViteConfig(aeroConfig, {
-	command: process.argv.includes('build') ? 'build' : 'dev',
-	mode: (process.env.NODE_ENV as 'development' | 'production') || 'development',
+export default defineConfig({
+	plugins: aero(),
 })
